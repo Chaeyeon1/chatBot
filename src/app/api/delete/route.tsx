077e46d2c8ea22691a36e378627e8d8ed74394f3
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-13 02:36:27 +0900

    GET test

--- src/app/api/delete/route.tsx
+++ src/app/api/delete/route.tsx
@@ -14,3 +14,14 @@ export async function POST(request: Request) {
 
   return NextResponse.json(deletedUser);
 }
+
+export async function GET(request: Request) {
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
