9b25bdc28417166312902055c57640838d273746
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-13 03:04:40 +0900

    콘솔 추가

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -57,6 +57,7 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 export async function GET() {
+  console.log("asdfasdf");
   const allUsers = await client.user.findMany();
 
   return NextResponse.json(allUsers);
